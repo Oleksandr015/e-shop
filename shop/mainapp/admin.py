94feae56993a5da4a550ebf4b594794942a0f8da
Oleksandr <iskander@kursGit.pl>
2021-02-09 18:05:59 +0100

    Delete NotebookAdminForm in admin.py

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,10 +1,6 @@
-from PIL import Image
-
-from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
 from .models import *
-
-from django.utils.safestring import mark_safe
 
 
 class SmartphoneAdminForm(ModelForm):
@@ -22,30 +18,7 @@ class SmartphoneAdminForm(ModelForm):
         return self.cleaned_data
 
 
-class NotebookAdminForm(ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = mark_safe(
-            '<span style="color:red; font_size:14px;">Minimalny rozmiar zdjecia {}x{}</span>'.format(
-                *Product.MIN_RESOLUTION))
-
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = Product.MIN_RESOLUTION
-        max_height, max_width = Product.MIN_RESOLUTION
-        if image.size > Product.MAX_IMAGE_SIZE:
-            raise ValidationError('Rozmiar zdjecia nie moze byc wiekszy za 3 Mb')
-        if img.height < min_height or img.weihgt < min_width:
-            raise ValidationError('Rozmiar zdjecia mniejszy za minimalne')
-        if img.height > max_height or img.weihgt > max_width:
-            raise ValidationError('Rozmiar zdjecia wiekszy za maksymalne')
-        return image
-
-
 class NotebookAdmin(admin.ModelAdmin):
-    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
