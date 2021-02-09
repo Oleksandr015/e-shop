82a135eb986c09fbbd4c8d5033ec3b34c56bb993
Oleksandr <iskander@kursGit.pl>
2021-02-09 18:13:14 +0100

    Make change in base.html

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -77,6 +77,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
 
 class Product(models.Model):
 
