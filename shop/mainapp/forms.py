0e38d5b46b928d5a46f12a72c2e3ad76a7f5edb9
Oleksandr <iskander@kursGit.pl>
2021-04-13 16:10:58 +0200

    Create login form

--- shop/mainapp/forms.py
+++ shop/mainapp/forms.py
@@ -1,10 +1,10 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
-
     order_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
 
     def __init__(self, *args, **kwargs):
@@ -16,3 +16,23 @@ class OrderForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone', 'address', 'buying_type', 'order_date', 'comment'
         )
+
+
+class LoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Login'
+        self.fields['password'].label = 'Hasło'
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Użytkownika z loginem {username} nie istnieje!')
+        user = User.objects.filter(username).first()
+        if user:
+            if not user.check_password(password):
+                raise forms.ValidationError("Neprawidlowe hasło")
+        return self.cleaned_data
