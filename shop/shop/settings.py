5c6778bbd99c6f5b0b3c8c2f1e0f6f60ec693ade
Oleksandr <iskander@kursGit.pl>
2021-04-16 15:04:46 +0200

    Remove database to postgres

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecom_db',
+        'USER': 'ecouser',
+        'PASSWORD': 'devpass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
