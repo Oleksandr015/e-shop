696be356974f83b0437fc2837c95eaeed2c729ae
Oleksandr <iskander@kursGit.pl>
2021-04-16 16:46:36 +0200

    Add new models

--- shop/specs/admin.py
+++ shop/specs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import  *
 
-# Register your models here.
+admin.site.registration(ProductFeatures)
+admin.site.registration(CategoryFeature)
+admin.site.registration(FeatureValidator)
+
+
