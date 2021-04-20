502232fdddafed9d710e29c42b3093b9e5815057
Oleksandr <iskander@kursGit.pl>
2021-04-20 14:55:19 +0200

    Registered models

--- shop/specs/admin.py
+++ shop/specs/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import  *
+from .models import *
 
-admin.site.registration(ProductFeatures)
-admin.site.registration(CategoryFeature)
-admin.site.registration(FeatureValidator)
+admin.site.register(ProductFeatures)
+admin.site.register(CategoryFeature)
+admin.site.register(FeatureValidator)
 
 
