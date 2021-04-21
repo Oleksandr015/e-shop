ce8b5cd634133f6e8ecb28a7c6855d2363b514ca
Oleksandr <iskander@kursGit.pl>
2021-04-21 11:49:02 +0200

    Continuation of work on the Admin panel of characteristics

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -128,11 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATIC_DIRS = (os.path.join(BASE_DIR, 'static_dev'))
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- shop/specs/forms.py
+++ shop/specs/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from mainapp.models import Category
+from .models import CategoryFeature, FeatureValidator
 
 
 class NewCategoryForm(forms.ModelForm):
@@ -7,3 +8,24 @@ class NewCategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class NewCategoryFeatureKeyForm(forms.ModelForm):
+
+    class Meta:
+        model = CategoryFeature
+        fields = '__all__'
+
+
+class NewCategoryForm(forms.ModelForm):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class FeatureValidatorForm(forms.ModelForm):
+
+    class Meta:
+        model = FeatureValidator
+        fields = ['category']
